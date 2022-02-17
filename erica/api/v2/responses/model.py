a1682c8dce9c13f8a0f4f6c8ed45bb99ed0500d7
VicCamp <victor.delcampo@digitalservice.bund.de>
2022-02-17 13:11:02 +0100

    STL-1821 | Removed code smell

--- erica/api/v2/responses/model.py
+++ erica/api/v2/responses/model.py
@@ -2,6 +2,9 @@ from erica.request_processing.erica_input.v2.erica_input import ErrorRequestQueu
     ResponseGetSendEstFromQueue, ResponseGetTaxNumberValidityFromQueue, \
     ResponseGetUnlockCodeRequestAndActivationFromQueue, ResponseGetUnlockCodeRevocationFromQueue
 
+model_error_request_queue = {"model": ErrorRequestQueue,
+                             "description": "Job status could not be retrieved from the queue."}
+
 response_model_post_to_queue = {
     201: {"description": "Job was successfully submitted to the queue and the job id was returned."},
     422: {"model": ErrorRequestQueue, "description": "Job could not be submitted to the queue."}}
@@ -9,29 +12,29 @@ response_model_post_to_queue = {
 response_model_get_est_validation_from_queue = {
     200: {"model": ResponseGetFromQueue,
           "description": "Job status of an est validation was successfully retrieved from the queue."},
-    500: {"model": ErrorRequestQueue, "description": "Job status could not be retrieved from the queue."}}
+    500: model_error_request_queue}
 
 response_model_get_send_est_from_queue = {
     200: {"model": ResponseGetSendEstFromQueue,
           "description": "Job status of a sent est was successfully retrieved from the queue."},
-    500: {"model": ErrorRequestQueue, "description": "Job status could not be retrieved from the queue."}}
+    500: model_error_request_queue}
 
 response_model_get_tax_number_validity_from_queue = {
     200: {"model": ResponseGetTaxNumberValidityFromQueue,
           "description": "Job status of a tax number validity was successfully retrieved from the queue."},
-    500: {"model": ErrorRequestQueue, "description": "Job status could not be retrieved from the queue."}}
+    500: model_error_request_queue}
 
 response_model_get_unlock_code_request_from_queue = {
     200: {"model": ResponseGetUnlockCodeRequestAndActivationFromQueue,
           "description": "Job status of an unlock code request was successfully retrieved from the queue."},
-    500: {"model": ErrorRequestQueue, "description": "Job status could not be retrieved from the queue."}}
+    500: model_error_request_queue}
 
 response_model_get_unlock_code_activation_from_queue = {
     200: {"model": ResponseGetUnlockCodeRequestAndActivationFromQueue,
           "description": "Job status of an unlock code activation was successfully retrieved from the queue."},
-    500: {"model": ErrorRequestQueue, "description": "Job status could not be retrieved from the queue."}}
+    500: model_error_request_queue}
 
 response_model_get_unlock_code_revocation_from_queue = {
     200: {"model": ResponseGetUnlockCodeRevocationFromQueue,
           "description": "Job status of an unlock code revocation was successfully retrieved from the queue."},
-    500: {"model": ErrorRequestQueue, "description": "Job status could not be retrieved from the queue."}}
+    500: model_error_request_queue}
