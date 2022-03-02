838fee5d0c7340728666b3237e3b7fc64f2ee034
jo punknoir <joerdis.schuller@digitalservice.bund.de>
2022-03-02 09:08:24 +0100

    feat(api): add routes for tax validation and fsc

--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,12 @@
 from uuid import UUID
 from fastapi import FastAPI, Depends
+
+from src.application.freischalt_code import FreischaltCodeDto, FreischaltCodeCreateDto
+from src.application.freischalt_code_service import FreischaltCodeService
 from src.application.tax_declaration_service import TaxDeclarationService
-from src.application.tax_declaration import TaxDeclarationDto, TaxDeclarationCreateDto
+from src.application.tax_declaration import TaxDeclarationDto, TaxDeclarationCreateDto, TaxDeclarationValidateDto
 from src.infrastructure.sqlalchemy.database import run_migrations
+from src.infrastructure.sqlalchemy.repositories.freischalt_code_repository import FreischaltCodeRepository
 from src.infrastructure.sqlalchemy.repositories.tax_declaration_repository import TaxDeclarationRepository
 from fastapi_versioning import VersionedFastAPI, version
 
@@ -37,6 +41,48 @@ async def create_tax_declaration(
         tax_declaration_service: TaxDeclarationService = Depends(TaxDeclarationService)
 ):
     result = tax_declaration_service.create(tax_declaration_create_dto)
+    return result
+
+
+@app.get('/tax_validations')
+@version(1, 0)
+async def get_tax_validations():
+    pass
+
+
+@app.get('/tax_validations/{id}')
+@version(1, 0)
+async def get_tax_validation():
+    pass
+
+
+@app.post('/tax_validations', response_model=TaxDeclarationValidateDto)
+@version(1, 0)
+async def create_tax_validation():
+    pass
+
+
+@app.get("/freischalt_codes")
+@version(1, 0)
+async def get_freischalt_codes(skip: int, limit: int,
+                               repo: FreischaltCodeRepository = Depends(FreischaltCodeRepository)):
+    return repo.get(skip, limit)
+
+
+@app.get("/freischalt_codes/{id}")
+@version(1, 0)
+async def get_freischalt_code(entity_id: UUID,
+                              freischalt_code_service: FreischaltCodeService = Depends(FreischaltCodeService)):
+    return freischalt_code_service.get_status(entity_id)
+
+
+@app.post("/freischalt_codes", response_model=FreischaltCodeDto)
+@version(1, 0)
+async def create_freischalt_code(
+        freischalt_code_create_dto: FreischaltCodeCreateDto,
+        freischalt_code_service: FreischaltCodeService = Depends(FreischaltCodeService)
+):
+    result = freischalt_code_service.create(freischalt_code_create_dto)
     return result
 
 
