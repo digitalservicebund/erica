09222c69158a0ab8f462e9773e74fd09e68534fd
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-03-07 11:14:35 +0100

    Adapt transfer header tests

--- tests/elster_xml/common/test_transfer_header.py
+++ tests/elster_xml/common/test_transfer_header.py
@@ -9,6 +9,8 @@ from erica.pyeric.eric_errors import EricProcessNotSuccessful
 from tests.utils import missing_cert, missing_pyeric_lib, remove_declaration_and_namespace
 
 
+@pytest.mark.skipif(missing_cert(), reason="skipped because of missing cert.pfx; see pyeric/README.md")
+@pytest.mark.skipif(missing_pyeric_lib(), reason="skipped because of missing eric lib; see pyeric/README.md")
 class TestGenerateTransferHeader:
 
     @pytest.fixture
@@ -32,11 +34,7 @@ class TestGenerateTransferHeader:
             datenLieferant='Softwaretester ERiC',
         )
 
-    @pytest.mark.skipif(missing_cert(), reason="skipped because of missing cert.pfx; see pyeric/README.md")
-    @pytest.mark.skipif(missing_pyeric_lib(), reason="skipped because of missing eric lib; see pyeric/README.md")
-    def test_if_correct_input_then_transfer_header_is_added_and_reference_unchanged(self, correct_input_xml, th_fields):
-        input_xml_before_use = copy.deepcopy(correct_input_xml)
-
+    def test_if_correct_input_then_transfer_header_is_added(self, correct_input_xml, th_fields):
         result = add_transfer_header(correct_input_xml, th_fields)
 
         assert "TransferHeader" in result
@@ -45,16 +43,18 @@ class TestGenerateTransferHeader:
         assert "<HerstellerID>" + th_fields.herstellerId + "</HerstellerID>" in result
         assert "<DatenLieferant>" + th_fields.datenLieferant + "</DatenLieferant>" in result
 
+    def test_if_correct_input_then_reference_unchanged(self, correct_input_xml, th_fields):
+        input_xml_before_use = copy.deepcopy(correct_input_xml)
+
+        add_transfer_header(correct_input_xml, th_fields)
+
         assert correct_input_xml == input_xml_before_use
 
-    @pytest.mark.skipif(missing_cert(), reason="skipped because of missing cert.pfx; see pyeric/README.md")
-    @pytest.mark.skipif(missing_pyeric_lib(), reason="skipped because of missing eric lib; see pyeric/README.md")
     def test_if_incorrect_input_then_raise_not_successful_error(self, incorrect_input_xml, th_fields):
         with pytest.raises(EricProcessNotSuccessful):
             add_transfer_header(incorrect_input_xml, th_fields)
 
-    @pytest.mark.skipif(missing_pyeric_lib(), reason="skipped because of missing eric lib; see pyeric/README.md")
-    def test_generate_transfer_header_calls_run_pyeric(self, th_fields):
+    def test_calls_run_pyeric_with_correct_arguments(self, th_fields):
         xml = "<xml/>"
         xml_with_th_binary = '<xml>This includes the transfer header.</xml>'.encode()
         with patch('erica.pyeric.eric.EricWrapper.create_th',
@@ -67,12 +67,12 @@ class TestGenerateTransferHeader:
                                              verfahren=th_fields.verfahren,
                                              datenLieferant=th_fields.datenLieferant)
 
-    @pytest.mark.skipif(missing_pyeric_lib(), reason="skipped because of missing eric lib; see pyeric/README.md")
-    def test_generate_transfer_header_returns_xml_with_transfer_header(self, th_fields):
+    def test_returns_decoded_result_of_eric_th_function(self, th_fields):
         xml = "<xml/>"
-        xml_with_th_binary = '<xml>This includes the transfer header.</xml>'.encode()
+        xml_with_th_binary = '<xml>This includes the transfer header.</xml>'
+
         with patch('erica.pyeric.eric.EricWrapper.create_th',
-                   MagicMock(return_value=xml_with_th_binary)):
+                   MagicMock(return_value=xml_with_th_binary.encode())):
             res = add_transfer_header(xml, th_fields)
 
-            assert res == xml_with_th_binary.decode()
+            assert res == xml_with_th_binary
