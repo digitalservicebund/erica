4881312faf3e3f46b6c800a7af8a92ab1e61dcb6
jo punknoir <joerdis.schuller@digitalservice.bund.de>
2022-03-15 20:35:52 +0100

    refactor: remove commented out routes from main

--- erica/api/main.py
+++ erica/api/main.py
@@ -8,7 +8,6 @@ from erica.application.EricaAuftrag.EricaAuftragService import EricaAuftragServi
 from erica.application.FreischaltCode.FreischaltCode import FreischaltCodeBeantragenDto
 
 from erica.application.FreischaltCode.FreischaltCodeService import FreischaltCodeServiceInterface
-from erica.application.TaxDeclaration.TaxDeclaration import TaxDeclarationValidateDto
 from erica.infrastructure.sqlalchemy.database import run_migrations
 from fastapi_versioning import VersionedFastAPI, version
 
@@ -26,47 +25,6 @@ app = FastAPI(
 injector = Injector([
     ApiModule(),
 ])
-
-# @app.get("/tax_declarations")
-# @version(1, 0)
-# async def get_tax_declarations(skip: int, limit: int,
-#                                repo: TaxDeclarationRepository = Depends(TaxDeclarationRepository)):
-#     return repo.get(skip, limit)
-#
-#
-# @app.get("/tax_declarations/{id}")
-# @version(1, 0)
-# async def get_tax_declaration(entity_id: UUID,
-#                               tax_declaration_service: TaxDeclarationService = Depends(TaxDeclarationService)):
-#     return tax_declaration_service.get_status(entity_id)
-#
-#
-# @app.post("/tax_declarations", response_model=TaxDeclarationDto)
-# @version(1, 0)
-# async def create_tax_declaration(
-#         tax_declaration_create_dto: TaxDeclarationCreateDto,
-#         tax_declaration_service: TaxDeclarationService = Depends(TaxDeclarationService)
-# ):
-#     result = tax_declaration_service.create(tax_declaration_create_dto)
-#     return result
-
-
-@app.get('/tax_validations')
-@version(1, 0)
-async def get_tax_validations():
-    pass
-
-
-@app.get('/tax_validations/{id}')
-@version(1, 0)
-async def get_tax_validation():
-    pass
-
-
-@app.post('/tax_validations', response_model=TaxDeclarationValidateDto)
-@version(1, 0)
-async def create_tax_validation():
-    pass
 
 
 @app.get("/erica_auftraege")
@@ -89,42 +47,6 @@ async def create_freischalt_code(freischalt_code_beantragen_dto: FreischaltCodeB
     freischalt_code_service: FreischaltCodeServiceInterface = injector.inject(FreischaltCodeServiceInterface)
     result = await freischalt_code_service.freischalt_code_bei_elster_beantragen_queued(freischalt_code_beantragen_dto)
     return result
-
-
-# @app.get("/freischalt_code_activate/{id}")
-# @version(1, 0)
-# async def get_freischalt_code_activate(entity_id: UUID,
-#                                        freischalt_code_activation_service: FreischaltCodeActivationService =
-#                                        Depends(FreischaltCodeActivationService)):
-#     return freischalt_code_activation_service.get_status(entity_id)
-#
-#
-# @app.post("/freischalt_code_activate", response_model=FreischaltCodeActivateDto)
-# @version(1, 0)
-# async def create_freischalt_code_activate(
-#         freischalt_code_create_activate_dto: FreischaltCodeCreateActivateDto,
-#         freischalt_code_activation_service: FreischaltCodeActivationService = Depends(FreischaltCodeActivationService)
-# ):
-#     result = freischalt_code_activation_service.create(freischalt_code_create_activate_dto)
-#     return result
-#
-#
-# @app.get("/freischalt_code_revocate/{id}")
-# @version(1, 0)
-# async def get_freischalt_code_revocate(entity_id: UUID,
-#                                        freischalt_code_service: FreischaltCodeRevocationService =
-#                                        Depends(FreischaltCodeRevocationService)):
-#     return freischalt_code_service.get_status(entity_id)
-#
-#
-# @app.post("/freischalt_code_revocate", response_model=FreischaltCodeRevocateDto)
-# @version(1, 0)
-# async def create_freischalt_code_revocate(
-#         freischalt_code_create_revocate_dto: FreischaltCodeCreateRevocateDto,
-#         freischalt_code_revocation_service: FreischaltCodeRevocationService = Depends(FreischaltCodeRevocationService)
-# ):
-#     result = freischalt_code_revocation_service.create(freischalt_code_create_revocate_dto)
-#     return result
 
 
 app = VersionedFastAPI(app)
