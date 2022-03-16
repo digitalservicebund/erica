32b922ccad02512aed5b32de35151142f0a3ff04
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-03-16 22:32:10 +0100

    Restructure queue creation

--- erica/infrastructure/rq/queue.py
+++ erica/infrastructure/rq/queue.py
@@ -3,7 +3,15 @@ from rq import Queue, Connection
 
 from erica.erica_legacy.config import get_settings
 
-with Connection(Redis(get_settings().queue_host, get_settings().queue_port)):
-    dongle_queue = Queue('dongle')
-    cert_queue = Queue('cert')
-    common_queue = Queue('common')
+_ALLOWED_QUEUE_NAMES = ['dongle', 'cert', 'common']
+
+
+class QueueNotAvailableError(Exception):
+    """Raised in case an unexpected queue name was provided"""
+
+
+def get_queue(queue_name='dongle'):
+    if queue_name not in _ALLOWED_QUEUE_NAMES:
+        raise QueueNotAvailableError
+    with Connection(Redis(get_settings().queue_host, get_settings().queue_port)):
+        return Queue(queue_name)
