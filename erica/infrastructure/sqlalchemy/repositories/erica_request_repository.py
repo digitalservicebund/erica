49e6301d474ff7970f8d4426cdb944b1a1e95aad
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-03-21 16:52:53 +0100

    Add methods to erica_request_repository

--- erica/infrastructure/sqlalchemy/repositories/erica_request_repository.py
+++ erica/infrastructure/sqlalchemy/repositories/erica_request_repository.py
@@ -1,11 +1,13 @@
 from abc import ABC
+from uuid import UUID
 
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from erica.domain.erica_request.erica_request import EricaRequest
 from erica.domain.Repositories.EricaRequestRepositoryInterface import EricaRequestRepositoryInterface
 from erica.infrastructure.sqlalchemy.erica_request_schema import EricaRequestSchema
-from erica.infrastructure.sqlalchemy.repositories.base_repository import BaseRepository
+from erica.infrastructure.sqlalchemy.repositories.base_repository import BaseRepository, EntityNotFoundError
 
 
 class EricaRequestRepository(
@@ -17,3 +19,29 @@ class EricaRequestRepository(
         super().__init__(db_connection)
         self.DatabaseEntity = EricaRequestSchema
         self.DomainModel = EricaRequest
+
+    def get_by_job_id(self, job_id: UUID) -> EricaRequest:
+        entity = self._get_by_job_id(job_id).first()
+        if entity is None:
+            raise EntityNotFoundError
+        return self.DomainModel.from_orm(entity)
+
+    def _get_by_job_id(self, job_id: UUID):
+        entity = self.db_connection.query(self.DatabaseEntity).filter(self.DatabaseEntity.job_id == job_id)
+        return entity
+
+    def update_by_job_id(self, job_id: UUID, model: BaseModel) -> EricaRequest:
+        current = self._get_by_job_id(job_id)
+        current.update(model.dict())
+        self.db_connection.commit()
+
+        updated = self.get_by_job_id(job_id)
+        return self.DomainModel.from_orm(updated)
+
+    def delete_by_job_id(self, job_id: UUID):
+        entity = self._get_by_job_id(job_id).first()
+        if entity is None:
+            raise EntityNotFoundError
+
+        self.db_connection.delete(entity)
+        self.db_connection.commit()
