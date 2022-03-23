31ef322b8d3ef064510a75fdb8f7257f5b71630b
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-03-23 15:23:35 +0100

    Set bufa nr correctly

--- erica/erica_legacy/elster_xml/common/electronic_steuernummer.py
+++ erica/erica_legacy/elster_xml/common/electronic_steuernummer.py
@@ -1,6 +1,33 @@
+from erica.erica_legacy.elster_xml.est_mapping import generate_electronic_steuernummer
 from erica.erica_legacy.pyeric.eric import get_eric_wrapper
+
+BUNDESLAENDER_WITH_STEUERNUMMER = ["BE", "HB", "SH"]
+
+
+def get_bufa_nr(steuernummer: str, bundesland: str, steuernummer_could_be_aktenzeichen=False):
+    """
+        Returns the bufa-nr for the given steuernummer/aktenzeichen and bundesland.
+        :param steuernummer: steuernummer/aktenzeichen in state-specific format
+        :param bundesland: bundesland identifier
+        :param steuernummer_could_be_aktenzeichen: In some cases the given steuernummer can be an aktenzeichen instead.
+        If True, we differentiate if the given bundesland has steuernummer or aktenzeichen as their identifier.
+     """
+
+    if steuernummer_could_be_aktenzeichen and bundesland not in BUNDESLAENDER_WITH_STEUERNUMMER:
+        electronic_steuernummer = generate_electronic_aktenzeichen(steuernummer, bundesland)
+    else:
+        electronic_steuernummer = generate_electronic_steuernummer(steuernummer, bundesland)
+
+    bufa_nr = electronic_steuernummer[:4]
+    return bufa_nr
 
 
 def generate_electronic_aktenzeichen(aktenzeichen, bundesland):
+    """
+        Returns the elster-specific format for the given aktenzeichen and bundesland.
+        :param aktenzeichen: aktenzeichen in state-specific format
+        :param bundesland: bundesland identifier
+     """
+    
     with get_eric_wrapper() as eric_wrapper:
         return eric_wrapper.get_electronic_aktenzeichen(aktenzeichen, bundesland)
--- tests/erica_legacy/elster_xml/common/test_electronic_steuernummer.py
+++ tests/erica_legacy/elster_xml/common/test_electronic_steuernummer.py
@@ -1,7 +1,39 @@
+from unittest.mock import patch
+
 import pytest
 
-from erica.erica_legacy.elster_xml.common.electronic_steuernummer import generate_electronic_aktenzeichen
+from erica.erica_legacy.elster_xml.common.electronic_steuernummer import generate_electronic_aktenzeichen, get_bufa_nr
 from tests.erica_legacy.utils import missing_pyeric_lib
+
+
+@pytest.mark.skipif(missing_pyeric_lib(), reason="skipped because of missing eric lib; see pyeric/README.md")
+class TestGetBufaNr:
+    def test_if_steuernummer_cannot_be_aktenzeichen_then_return_bufa_correctly(self):
+        result = get_bufa_nr("13381508159", "NW")
+        assert result == "5133"
+
+    def test_if_steuernummer_cannot_be_aktenzeichen_then_construct_steuernummer(self):
+        with patch("erica.erica_legacy.elster_xml.common.electronic_steuernummer.generate_electronic_steuernummer") as fun_gen_steuernummer:
+            get_bufa_nr("2181508150", "BE")
+            fun_gen_steuernummer.assert_called_once()
+
+    def test_if_steuernummer_can_be_aktenzeichen_and_no_special_bundesland_then_return_bufa_correctly(self):
+        result = get_bufa_nr("2080353038893", "NW", steuernummer_could_be_aktenzeichen=True)
+        assert result == "5208"
+
+    def test_if_steuernummer_can_be_aktenzeichen_and_no_special_bundesland_then_construct_aktenzeichen(self):
+        with patch("erica.erica_legacy.elster_xml.common.electronic_steuernummer.generate_electronic_aktenzeichen") as fun_gen_aktenzeichen:
+            get_bufa_nr("2080353038893", "NW", steuernummer_could_be_aktenzeichen=True)
+            fun_gen_aktenzeichen.assert_called_once()
+
+    def test_if_steuernummer_can_be_aktenzeichen_and_special_bundesland_then_return_bufa_correctly(self):
+        result = get_bufa_nr("2181508150", "BE", steuernummer_could_be_aktenzeichen=True)
+        assert result == "1121"
+
+    def test_if_steuernummer_can_be_aktenzeichen_and_special_bundesland_then_construct_steuernummer(self):
+        with patch("erica.erica_legacy.elster_xml.common.electronic_steuernummer.generate_electronic_steuernummer") as fun_gen_steuernummer:
+            get_bufa_nr("2181508150", "BE", steuernummer_could_be_aktenzeichen=True)
+            fun_gen_steuernummer.assert_called_once()
 
 
 @pytest.mark.skipif(missing_pyeric_lib(), reason="skipped because of missing eric lib; see pyeric/README.md")
