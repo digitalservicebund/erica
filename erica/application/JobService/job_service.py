1017e1b27a59c08850b0a21f95f15886dbdad1b5
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-03-24 10:48:20 +0100

    Add correct request UUID column
    This reverts commit f7dcb7e5

--- erica/application/JobService/job_service.py
+++ erica/application/JobService/job_service.py
@@ -46,7 +46,7 @@ class JobService(JobServiceInterface):
         self.job_method = job_method
 
     def add_to_queue(self, payload_dto: BaseDto, job_type: RequestType) -> EricaAuftragDto:
-        request_entity = EricaRequest(job_id=uuid4(),
+        request_entity = EricaRequest(request_id=uuid4(),
                                       payload=self.payload_type.parse_obj(payload_dto),
                                       creator_id="api",
                                       type=job_type
@@ -55,9 +55,8 @@ class JobService(JobServiceInterface):
         created = self.repository.create(request_entity)
 
         self.background_worker.enqueue(
-            created.id,
-            f=self.job_method,
-            job_id=request_entity.job_id.__str__(),
+            created.request_id,
+            f=self.job_method
         )
 
         return EricaAuftragDto.parse_obj(created)
