68ab24d72b8f739426b926a9dfdd1543e56ad706
VicCamp <victor.delcampo@digitalservice.bund.de>
2022-03-29 16:19:55 +0200

    NO_TICKET | entrypoint syntax corrected

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,9 @@ set -ex
 
 service pcscd start
 
-
-if [[ -z "${RUN_ONLY_API}" ]]; then
+if [ -z "$RUN_ONLY_API" ]; then
   exec pipenv run "$@"
-elif [[ -z "${RUN_ONLY_WORKER}" ]]; then
+elif [ -z "$RUN_ONLY_WORKER" ]; then
   pipenv run python -m erica.infrastructure.rq.worker&
 else
   pipenv run python -m erica.infrastructure.rq.worker&
