d0131d44bc22750bd043ef65d2d640bc9fc4a75a
VicCamp <victor.delcampo@digitalservice.bund.de>
2022-04-05 08:51:40 +0200

    STL-2050 | Fix merge conflicts

--- erica/application/JobService/job_service.py
+++ erica/application/JobService/job_service.py
@@ -7,7 +7,6 @@ from erica.application.base_dto import BaseDto
 from erica.application.erica_request.erica_request import EricaRequestDto
 from erica.domain.BackgroundJobs.BackgroundJobInterface import BackgroundJobInterface
 from erica.domain.Shared.BaseDomainModel import BasePayload
-from erica.domain.Shared.EricaRequest import RequestType
 from erica.domain.erica_request.erica_request import EricaRequest
 from erica.domain.repositories.erica_request_repository_interface import EricaRequestRepositoryInterface
 from erica.domain.Shared.EricaRequest import RequestType
