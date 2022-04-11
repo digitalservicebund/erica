68299734daabac87e5664ec6f569f4515854274f
VicCamp <victor.delcampo@digitalservice.bund.de>
2022-04-11 10:30:38 +0200

    NO_TICKET | Sleep time for job-checking-result tests raised to 10 seconds

--- contract_tests/test_integration_queue.py
+++ contract_tests/test_integration_queue.py
@@ -173,7 +173,7 @@ class TestV2UnlockCodeRequest:
                                  data=json.dumps(full_unlock_code_request_data, default=str))
         assert response.status_code == 201
         uuid = response.headers['Location'].split("/")[3]
-        sleep(4)
+        sleep(10)
         response = requests.get(ERICA_TESTING_URL + self.endpoint + "/" + uuid)
 
         assert response.status_code == 200
@@ -237,7 +237,7 @@ class TestV2UnlockCodeActivation:
                                  data=json.dumps(full_unlock_code_activation_data, default=str))
         assert response.status_code == 201
         uuid = response.headers['Location'].split("/")[3]
-        sleep(4)
+        sleep(10)
         response = requests.get(ERICA_TESTING_URL + self.endpoint + "/" + uuid)
 
         assert response.status_code == 200
@@ -301,7 +301,7 @@ class TestV2UnlockCodeRevocation:
                                  data=json.dumps(full_unlock_code_revocation_data, default=str))
         assert response.status_code == 201
         uuid = response.headers['Location'].split("/")[3]
-        sleep(4)
+        sleep(10)
         response = requests.get(ERICA_TESTING_URL + self.endpoint + "/" + uuid)
 
         assert response.status_code == 200
@@ -363,7 +363,7 @@ class TestV2TaxNumberValidity:
                                  data=json.dumps(tax_number_validity_data, default=str))
         assert response.status_code == 201
         uuid = response.headers['Location'].split("/")[2]
-        sleep(4)
+        sleep(10)
         response = requests.get(ERICA_TESTING_URL + self.endpoint + "/" + uuid)
 
         assert response.status_code == 200
@@ -437,7 +437,7 @@ class TestV2SendEst:
                                  data=json.dumps(full_est_data, default=str))
         assert response.status_code == 201
         uuid = response.headers['Location'].split("/")[2]
-        sleep(4)
+        sleep(10)
         response = requests.get(ERICA_TESTING_URL + self.endpoint + "/" + uuid)
 
         assert response.status_code == 200
@@ -502,7 +502,7 @@ class TestV2GrundsteuerRequest:
                                  data=json.dumps(request, default=json_default))
         assert response.status_code == 201
         uuid = response.headers['Location'].split("/")[2]
-        sleep(4)
+        sleep(10)
         response = requests.get(ERICA_TESTING_URL + self.endpoint + "/" + uuid)
         assert response.status_code == 200
         assert response.json()["processStatus"] == "Failure"
