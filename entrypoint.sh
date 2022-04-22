6600dbdab86b571ad57801dafa0be69914b33464
VicCamp <victor.delcampo@digitalservice.bund.de>
2022-04-22 13:39:23 +0200

    NO_TICKET | Remove not needed variables in entrypoint for cron job

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,7 @@ set -ex
 rm -f /app/.env
 touch /app/.env
 set +x  # Turn off command logging to avoid leaking secrets
-echo "PYTHONDONTWRITEBYTECODE=$PYTHONDONTWRITEBYTECODE" >> /app/.env
-echo "PYTHONUNBUFFERED=$PYTHONUNBUFFERED" >> /app/.env
 echo "ERICA_ENV=$ERICA_ENV" >> /app/.env
-echo "DONGLE_CONNECTED=$DONGLE_CONNECTED" >> /app/.env
-echo "ERICA_DATABASE_URL=$ERICA_DATABASE_URL" >> /app/.env
 set -x  # Turn command logging back on
 
 service pcscd start
