e5a08c11e38cf807b03123c4b836c1f020015a86
VicCamp <victor.delcampo@digitalservice.bund.de>
2022-04-22 13:07:29 +0200

    STL-2050 | Revert log level to debug

--- erica/infrastructure/sqlalchemy/cron/update_entities_utils.py
+++ erica/infrastructure/sqlalchemy/cron/update_entities_utils.py
@@ -21,7 +21,7 @@ def delete_success_fail_entities():
     eric_request_repo = injector.inject(EricaRequestRepository)
     entities_deleted = eric_request_repo.delete_success_fail_old_entities(
         get_settings().ttl_finished_request_entities_in_min)
-    logging.getLogger().info(
+    logging.getLogger().debug(
         str(entities_deleted) + " success/fail entities deleted at " + datetime.now().strftime("%H:%M:%S"))
 
 
@@ -31,7 +31,7 @@ def set_not_processed_entities_to_failed():
     erica_request_repo = injector.inject(EricaRequestRepository)
     entities_updated = erica_request_repo.set_not_processed_entities_to_failed(
         get_settings().ttl_processing_request_entities_in_min)
-    logging.getLogger().info(
+    logging.getLogger().debug(
         str(entities_updated) + " processing entities  set to failedat " + datetime.now().strftime("%H:%M:%S"))
 
 
