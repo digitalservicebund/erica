87924ccb50022f008aa8c78b03d2989bd973360e
VicCamp <victor.delcampo@digitalservice.bund.de>
2022-04-22 15:45:49 +0200

    NO_TICKET | Readded env variables in entrypoint for cron jobs

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,11 @@ set -ex
 rm -f /app/.env
 touch /app/.env
 set +x  # Turn off command logging to avoid leaking secrets
+echo "PYTHONDONTWRITEBYTECODE=$PYTHONDONTWRITEBYTECODE" >> /app/.env
+echo "PYTHONUNBUFFERED=$PYTHONUNBUFFERED" >> /app/.env
 echo "ERICA_ENV=$ERICA_ENV" >> /app/.env
+echo "ERICA_DATABASE_URL=$ERICA_DATABASE_URL" >> /app/.env
+echo "DONGLE_CONNECTED=$DONGLE_CONNECTED" >> /app/.env
 set -x  # Turn command logging back on
 
 service pcscd start
