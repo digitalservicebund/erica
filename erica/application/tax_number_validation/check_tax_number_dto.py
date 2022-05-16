066e2eb6bd2e04674661e86d6dc3e828dfd61412
Julian <julian.roesner@4germany.org>
2022-05-16 18:32:01 +0200

    Update erica/application/tax_number_validation/check_tax_number_dto.py

--- erica/application/tax_number_validation/check_tax_number_dto.py
+++ erica/application/tax_number_validation/check_tax_number_dto.py
@@ -2,7 +2,6 @@ from typing import Optional
 
 from erica.application.Shared.response_dto import ResponseBaseDto
 from erica.application.base_dto import BaseDto
-from erica.application.grundsteuer.camel_case_input import CamelCaseInput
 from erica.domain.tax_number_validation.check_tax_number import CheckTaxNumberPayload, StateAbbreviation
 
 
