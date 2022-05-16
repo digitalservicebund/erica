f28300b1eb0100dfed3eb54d9855df80b83d0360
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-05-16 16:34:12 +0200

    Use camelCase for errorMessages

--- erica/api/exception_handling.py
+++ erica/api/exception_handling.py
@@ -26,8 +26,8 @@ def generate_exception_handlers(app):
         logging.getLogger().info(f"The requested entity {request_id} is not present in the database.")
 
         return JSONResponse(
-            {"error_code": exc.__class__.__name__,
-             "error_message": f"The requested entity with id {request_id} was not found."},
+            {"errorCode": exc.__class__.__name__,
+             "errorMessage": f"The requested entity with id {request_id} was not found."},
             status_code=404,
         )
 
@@ -37,8 +37,8 @@ def generate_exception_handlers(app):
         logging.getLogger().info(
             f"The requested entity {request_id} was requested with the incorrect type {exc.requested_type}. Redirect to {redirection_url}")
         return JSONResponse(
-            {"error_code": exc.__class__.__name__,
-             "error_message": f"The actual location of the request id {request_id} is {redirection_url}"},
+            {"errorCode": exc.__class__.__name__,
+             "errorMessage": f"The actual location of the request id {request_id} is {redirection_url}"},
             status_code=404,
         )
 
@@ -46,8 +46,8 @@ def generate_exception_handlers(app):
         request_id = request.path_params.get('request_id')
         logging.getLogger().error(f"Request for entity {request_id} producted unexpected error: {str(exc)}")
         return JSONResponse(
-            {"error_code": "internal_server_error",
-             "error_message": "An unexpected error occurred."},
+            {"errorCode": "internal_server_error",
+             "errorMessage": "An unexpected error occurred."},
             status_code=500,
         )
 
@@ -69,8 +69,8 @@ def generate_exception_handlers(app):
             return await request_validation_exception_handler(request, exc)
         else:
             return JSONResponse(
-                {"error_code": exc.__class__.__name__,
-                 "error_message": exc.errors()},
+                {"errorCode": exc.__class__.__name__,
+                 "errorMessage": exc.errors()},
                 status_code=422,
             )
 
