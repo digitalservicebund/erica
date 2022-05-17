1d19573e61740eb3c8bde0e7499bc6b466d3da40
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-05-17 08:35:57 +0200

    Move camelCase model

--- erica/application/base_dto.py
+++ erica/application/base_dto.py
@@ -1,5 +1,27 @@
-from erica.application.grundsteuer.camel_case_input import CamelCaseInput
+from humps import camelize
+from pydantic import BaseModel, validator
 
 
-class BaseDto(CamelCaseInput):
+class CamelCaseModel(BaseModel):
+    class Config:
+        alias_generator = camelize
+        allow_population_by_field_name = True
+        anystr_strip_whitespace = True
+
+    @validator('*', pre=True)
+    def convert_empty_str_to_none_before(cls, v):
+        return cls.__convert_empty_str_to_none(v)
+
+    @validator('*')
+    def convert_empty_str_to_none_after(cls, v):
+        return cls.__convert_empty_str_to_none(v)
+
+    @staticmethod
+    def __convert_empty_str_to_none(v):
+        if v == "":
+            return None
+        return v
+
+
+class BaseDto(CamelCaseModel):
     pass
