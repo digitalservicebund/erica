fb0b24b00dd87662ea440aa0af67245927974bea
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-06-22 18:12:24 +0200

    Remove unused function

--- erica/domain/BackgroundJobs/BackgroundJobInterface.py
+++ erica/domain/BackgroundJobs/BackgroundJobInterface.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
+from rq.job import Job
+
 
 class BackgroundJobInterface:
     __metaclass__ = ABCMeta
@@ -14,8 +16,4 @@ class BackgroundJobInterface:
 
     @abstractmethod
     def get_enqueued_job_by_id(self, job_id):
-        pass
-
-    @abstractmethod
-    def list_all_jobs(self) -> Job:
         pass
