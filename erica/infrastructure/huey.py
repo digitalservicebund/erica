9eeba8339f3d984713d9854658c5c0d22c51a8d4
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-06-27 10:51:26 +0200

    Fix RedisHuey import

--- erica/infrastructure/huey.py
+++ erica/infrastructure/huey.py
@@ -1,5 +1,6 @@
+from huey import RedisHuey
+
 from erica.config import get_settings
 
-from erica.infrastructure.huey import RedisHuey
 
 huey = RedisHuey('erica-huey-queue', url=get_settings().queue_url, immediate=get_settings().use_immediate_worker)
