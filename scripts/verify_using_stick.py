1e4c7d12a278a261eed10187adac110333e165a5
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-07-04 16:24:04 +0200

    Update verify_using_stick.py to use huey

--- scripts/verify_using_stick.py
+++ scripts/verify_using_stick.py
@@ -1,3 +1,8 @@
-from erica.erica_legacy.pyeric.eric import verify_using_stick
+import os
 
-print(verify_using_stick())
+from erica.erica_legacy.pyeric.eric import verify_using_stick, verify_using_stick_with_queue
+
+if os.getenv("ENABLED_VERSION") == 'v2':
+    print(verify_using_stick_with_queue())
+else:
+    print(verify_using_stick())
