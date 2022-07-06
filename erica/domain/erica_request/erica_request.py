a24956eff63c7466b39de50cb438ae0dfe72965e
Niko Felger <niko.felger@gmail.com>
2022-07-06 17:57:38 +0200

    Improve EricaRequest __str__

--- erica/domain/erica_request/erica_request.py
+++ erica/domain/erica_request/erica_request.py
@@ -19,4 +19,4 @@ class EricaRequest(BaseDomainModel[UUID]):
         orm_mode = True
 
     def __str__(self):
-        return f"EricaRequest(type={self.type}, request_id={self.request_id}, status={self.status}"
+        return f"EricaRequest(type={self.type.name}, request_id={self.request_id}, status={self.status.name})"
