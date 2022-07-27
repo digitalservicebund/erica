e06311aa67253b2eea05f9ddab90799e15960d3d
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-07-27 11:14:52 +0200

    Extend list_permit script to be able to filter for one idnr

--- scripts/list_permits.py
+++ scripts/list_permits.py
@@ -1,12 +1,13 @@
 import os
-import click
+import sys
+from xml.etree.ElementTree import Element
 
 from erica.erica_legacy.elster_xml import elster_xml_generator
-from erica.erica_legacy.elster_xml.xml_parsing.erica_xml_parsing import remove_declaration_and_namespace
+from erica.erica_legacy.elster_xml.xml_parsing.erica_xml_parsing import remove_declaration_and_namespace, \
+    get_elements_from_xml_element, get_elements_text_from_xml_element
 from erica.erica_legacy.pyeric.pyeric_controller import PermitListingPyericProcessController
 
 
-@click.command()
 def get_idnr_status_list():
     xml = elster_xml_generator.generate_full_vast_list_xml()
 
@@ -14,10 +15,27 @@ def get_idnr_status_list():
 
     xml = remove_declaration_and_namespace(result.server_response)
     datenteil_xml = xml.find('.//DatenTeil')
-    print(elster_xml_generator._pretty(datenteil_xml))
+    return datenteil_xml
+
+
+def get_status_of_idnr(idnr, xml: Element):
+    antraege = get_elements_from_xml_element(xml, "Antrag")
+
+    matching_antraege = []
+    for antrag in antraege:
+        antrag_idnrs = get_elements_text_from_xml_element(antrag, "DateninhaberIdNr")
+        if (antrag_idnrs[0] == idnr):
+            matching_antraege.append(antrag)
+
+    for matching_antrag in matching_antraege:
+        print(elster_xml_generator._pretty(matching_antrag))
 
 
 if __name__ == "__main__":
     os.chdir('../')  # Change the working directory to be able to find the eric binaries
-    get_idnr_status_list()
-
+    requested_idnr = sys.argv[1] if len(sys.argv) > 1 else None
+    permit_list = get_idnr_status_list()
+    if requested_idnr:
+        get_status_of_idnr(requested_idnr, permit_list)
+    else:
+        print(elster_xml_generator._pretty(permit_list))
