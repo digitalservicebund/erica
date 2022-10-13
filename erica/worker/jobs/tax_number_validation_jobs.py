d05323bc6eba839bbc2eed4a2a4b2e8b9ae8046f
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-10-13 11:47:32 +0200

    Add expiration time to huey tasks

--- erica/worker/jobs/tax_number_validation_jobs.py
+++ erica/worker/jobs/tax_number_validation_jobs.py
@@ -6,7 +6,7 @@ from erica.domain.model.erica_request import RequestType
 from erica.domain.sqlalchemy.database import session_scope
 
 
-@huey.task()
+@huey.task(expires=120)
 def check_tax_number(request_id):
     from erica.job_service.job_service_factory import get_job_service
     with session_scope():
