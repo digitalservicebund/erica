6f111f4d8fd45def10e8a8f0e04201a8c7f9c777
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-12-05 09:35:46 +0100

    Increase job expiry to 4 minutes

--- erica/worker/jobs/grundsteuer_jobs.py
+++ erica/worker/jobs/grundsteuer_jobs.py
@@ -6,7 +6,7 @@ from erica.domain.model.erica_request import RequestType
 from erica.domain.sqlalchemy.database import session_scope
 
 
-@huey.task(expires=120)
+@huey.task(expires=240)
 def send_grundsteuer(request_id):
     with session_scope():
         from erica.job_service.job_service_factory import get_job_service
