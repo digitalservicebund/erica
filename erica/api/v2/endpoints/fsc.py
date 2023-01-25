9c91b2f11b871e0cb00dddadd1f88e92aefac70a
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2023-01-25 15:36:05 +0100

    Rename parameter

--- erica/api/v2/endpoints/fsc.py
+++ erica/api/v2/endpoints/fsc.py
@@ -17,14 +17,14 @@ router = APIRouter()
 
 
 @router.post('/request', status_code=status.HTTP_201_CREATED, responses=response_model_post_to_queue)
-async def request_fsc(request_fsc_client_identifier: FreischaltCodeRequestDto, request: Request):
+async def request_fsc(request_fsc_data: FreischaltCodeRequestDto, request: Request):
     """
     Route for requesting a new fsc for the sent id_nr using the job queue.
     :param request: API request object.
-    :param request_fsc_client_identifier: payload with client identifier and the JSON input data for the request.
+    :param request_fsc_data: payload with client identifier and the JSON input data for the request.
     """
     result = get_job_service(RequestType.freischalt_code_request).add_to_queue(
-        request_fsc_client_identifier.payload, request_fsc_client_identifier.client_identifier,
+        request_fsc_data.payload, request_fsc_data.client_identifier,
         RequestType.freischalt_code_request)
     return RedirectResponse(
         request.url_for("get_fsc_request_job", request_id=str(result.request_id)).removeprefix(str(request.base_url)),
@@ -43,14 +43,14 @@ async def get_fsc_request_job(request_id: UUID):
 
 
 @router.post('/activation', status_code=status.HTTP_201_CREATED, responses=response_model_post_to_queue)
-async def activate_fsc(activation_fsc_client_identifier: FreischaltCodeActivateDto, request: Request):
+async def activate_fsc(activation_fsc_data: FreischaltCodeActivateDto, request: Request):
     """
     Route for requesting activation of an fsc for the sent id_nr using the job queue.
     :param request: API request object.
-    :param activation_fsc_client_identifier: payload with client identifier and the JSON input data for the activation.
+    :param activation_fsc_data: payload with client identifier and the JSON input data for the activation.
     """
     result = get_job_service(RequestType.freischalt_code_activate).add_to_queue(
-        activation_fsc_client_identifier.payload, activation_fsc_client_identifier.client_identifier,
+        activation_fsc_data.payload, activation_fsc_data.client_identifier,
         RequestType.freischalt_code_activate)
     return RedirectResponse(
         request.url_for("get_fsc_activation_job", request_id=str(result.request_id)).removeprefix(
@@ -70,14 +70,14 @@ async def get_fsc_activation_job(request_id: UUID):
 
 
 @router.post('/revocation', status_code=status.HTTP_201_CREATED, responses=response_model_post_to_queue)
-async def revocate_fsc(revocation_fsc_client_identifier: FreischaltCodeRevocateDto, request: Request):
+async def revocate_fsc(revocation_fsc_data: FreischaltCodeRevocateDto, request: Request):
     """
     Route for requesting revocation of an fsc for the sent id_nr using the job queue.
     :param request: API request object.
-    :param revocation_fsc_client_identifier: payload with client identifier and the JSON input data for the revocation.
+    :param revocation_fsc_data: payload with client identifier and the JSON input data for the revocation.
     """
     result = get_job_service(RequestType.freischalt_code_revocate).add_to_queue(
-        revocation_fsc_client_identifier.payload, revocation_fsc_client_identifier.client_identifier,
+        revocation_fsc_data.payload, revocation_fsc_data.client_identifier,
         RequestType.freischalt_code_revocate)
     return RedirectResponse(
         request.url_for("get_fsc_revocation_job", request_id=str(result.request_id)).removeprefix(
